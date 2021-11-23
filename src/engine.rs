5a454a00f9f709fbf53d40f00f7d15e320b63019
Your Name <glen53simos53@gmail.com>
2021-11-22 23:58:17 -0700

    added board state wrapper, fen parsing, and board printing with unicode

---
+++ src/engine.rs
@@ -0,0 +1,196 @@
+use crate::piececonstants;
+use crate::pieceinit;
+
+lazy_static! { //allows me to use this stuff as statics, neat
+    static ref SLIDER_ATTACKS: Vec<u64>= pieceinit::init_slider_attacks2().0;
+}
+
+//          Bit macros
+macro_rules! get_bit {
+    //returns either 1 or 0, depending on if the square is a active bit
+    ($bb:expr, $square:expr) => {
+        if ($bb & 1u64 << $square) != 0 {
+            1
+        } else {
+            0
+        } // if statement checks if the and operator
+          // between bitboard and square is non zero, checks and returns square bit value
+    };
+}
+macro_rules! set_bit {
+    //sets a bit on a board to a 1
+    ($bb:expr, $square:expr) => {
+        $bb |= 1u64 << $square // takes the or between the bitboard the the shifted square number
+    };
+}
+macro_rules! pop_bit {
+    //sets a bit on a board to a 0
+    ($bb:expr, $square:expr) => {
+        $bb &= !(1u64 << $square) // takes the nand between the bitboard and the shifted square
+    };
+}
+pub struct Board {
+    pub pieceboards: [u64; 12], // [P, N, B, R, Q, K, p, n, b, r, q, k]
+    pub occupancies: [u64; 3],  // [white occupancies, black occupancies, total occupancies]
+    pub side: Option<u8>, // side to move, None for invalid, 0 for white, 1 for black (Color enum)
+    pub enpassant: usize, // enpassant possible square
+    pub castle: u8, // castling rights, 4 bits, 0001 1  white king kingside, 0010 2 white king queenside, 0100 4 black king king side, 1000 8 black king queen side
+}
+impl Default for Board {
+    fn default() -> Board {
+        Board {
+            pieceboards: [0; 12],                              // all boards to 0s
+            occupancies: [0; 3],                               // all occupancies to 0s
+            side: None,                                        // side no worky
+            enpassant: piececonstants::Square::NO_SQ as usize, // enpassant possible square, no_sq
+            castle: 0,
+        }
+    }
+}
+impl Board {
+    pub fn print_board(&self) -> () {
+        for rank in 0..8 {
+            for file in 0..8 {
+                if file == 0 {
+                    print!("{}  ", 8 - rank);
+                }
+                // init board square, turn file and rank into square
+                let square = rank * 8 + file;
+
+                let mut piece: Option<usize> = None;
+                for array in 0..12 {
+                    if get_bit!(self.pieceboards[array], square) == 1 {
+                        piece = Some(array);
+                    }
+                }
+                match piece {
+                    None => print!(". "),
+                    Some(number) => print!("{} ", piececonstants::UNICODE_PIECES[number]),
+                }
+            }
+            //print new line to seperate ranks
+            println!();
+        }
+        println!("   a b c d e f g h");
+        match self.side {
+            Some(0) => println!("Side:        White"),
+            Some(1) => println!("Side:        Black"),
+            _ => println!("Side:         None"),
+        }
+        println!(
+            "Enpassant:      {}",
+            piececonstants::SQUARE_TO_COORDINATES[self.enpassant]
+        );
+        println!("Castling      {:04b}", self.castle);
+    }
+    pub fn parse_fen(&mut self, fen: &str) {
+        self.pieceboards = [0; 12]; // all boards to 0s
+        self.occupancies = [0; 3]; // all occupancies to 0s
+        self.side = None; // side no worky
+        self.enpassant = piececonstants::Square::NO_SQ as usize; // enpassant possible square, no_sq
+        self.castle = 0;
+
+        let mut square = 0;
+
+        let split = fen.split(' ');
+        let segments: Vec<&str> = split.collect();
+
+        for c in segments[0].chars() {
+            match c.is_numeric() {
+                false => {
+                    let piece = match c {
+                        'P' => 0,
+                        'N' => 1,
+                        'B' => 2,
+                        'R' => 3,
+                        'Q' => 4,
+                        'K' => 5,
+                        'p' => 6,
+                        'n' => 7,
+                        'b' => 8,
+                        'r' => 9,
+                        'q' => 10,
+                        'k' => 11,
+                        '/' => continue,
+                        _ => 12,
+                    };
+                    if piece != 12 {
+                        set_bit!(self.pieceboards[piece], square);
+                        set_bit!(self.occupancies[2], square);
+                        if piece < 6 {
+                            set_bit!(self.occupancies[0], square);
+                        } else {
+                            set_bit!(self.occupancies[1], square);
+                        }
+                    }
+                    square += 1;
+                }
+                true => square += c as u32 - '0' as u32,
+            };
+        }
+        //w KQkq - 0 1
+
+        for c in segments[1].chars() {
+            self.side = match c {
+                'w' => Some(0),
+                'b' => Some(1),
+                _ => break,
+            };
+        }
+
+        for c in segments[2].chars() {
+            match c {
+                'K' => self.castle += piececonstants::Castling::wk as u8,
+                'Q' => self.castle += piececonstants::Castling::wq as u8,
+                'k' => self.castle += piececonstants::Castling::bk as u8,
+                'q' => self.castle += piececonstants::Castling::bq as u8,
+                '-' => (),
+                _ => break,
+            }
+        }
+
+        if segments[3] != "-" {
+            let chars: Vec<char> = segments[3].chars().collect();
+            let file = chars[0] as usize - 'a' as usize;
+            let rank = 8 - (chars[1] as usize - '0' as usize);
+            self.enpassant = rank * 8 + file;
+        }
+
+        //self.enpassant = piececonstants::Square::segments[3].unwrap()
+    }
+}
+//                                      bit manipulations
+
+pub fn get_bishop_attacks(square: usize, mut occupancy: u64) -> u64 {
+    occupancy &= piececonstants::BISHOP_MASKS[square];
+    occupancy = occupancy.wrapping_mul(piececonstants::BISHOPMAGICNUMBERS[square]);
+    occupancy >>= 64 - piececonstants::BISHOPBITS[square];
+
+    SLIDER_ATTACKS[piececonstants::BISHOP_POINTERS[square] + occupancy as usize]
+}
+
+pub fn get_rook_attacks(square: usize, mut occupancy: u64) -> u64 {
+    occupancy &= piececonstants::ROOK_MASKS[square];
+    occupancy = occupancy.wrapping_mul(piececonstants::ROOKMAGICNUMBERS[square]);
+    occupancy >>= 64 - piececonstants::ROOKBITS[square];
+
+    SLIDER_ATTACKS[piececonstants::ROOK_POINTERS[square] + occupancy as usize]
+}
+
+pub fn get_queen_attacks(square: usize, occupancy: u64) -> u64 {
+    let mut bishop_occupancy = occupancy;
+
+    let mut rook_occupancy = occupancy;
+
+    bishop_occupancy &= piececonstants::BISHOP_MASKS[square];
+    bishop_occupancy = bishop_occupancy.wrapping_mul(piececonstants::BISHOPMAGICNUMBERS[square]);
+    bishop_occupancy >>= 64 - piececonstants::BISHOPBITS[square];
+
+    rook_occupancy &= piececonstants::ROOK_MASKS[square];
+    rook_occupancy = rook_occupancy.wrapping_mul(piececonstants::ROOKMAGICNUMBERS[square]);
+    rook_occupancy >>= 64 - piececonstants::ROOKBITS[square];
+
+    SLIDER_ATTACKS[piececonstants::BISHOP_POINTERS[square] + bishop_occupancy as usize]
+        | SLIDER_ATTACKS[piececonstants::ROOK_POINTERS[square] + rook_occupancy as usize]
+    // returns rook + bishop attacks from square
+}
--- src/main.rs
+++ src/main.rs
@@ -1,79 +1,19 @@
-
-
-
-
 #[macro_use]
 extern crate lazy_static;
-
-
-mod pieceinit;
+#[macro_use]
+mod engine;
 mod piececonstants;
-
+mod pieceinit;
 //                                          enums and constants
 /*const NOTAFILE: u64 = 18374403900871474942; // masks giving 1s for all files but the edge files
 const NOTHFILE: u64 = 9187201950435737471; //probably not needed in this file
 const NOTHGFILE: u64 = 4557430888798830399;
 const NOTABFILE: u64 = 18229723555195321596;*/
-
-lazy_static! { //allows me to use this stuff as statics, neat
-    static ref SLIDER_ATTACKS: Vec<u64>= pieceinit::init_slider_attacks2().0;
-}
-
-enum Square {
-    A8, B8, C8, D8, E8, F8, G8, H8,
-    A7, B7, C7, D7, E7, F7, G7, H7,
-    A6, B6, C6, D6, E6, F6, G6, H6,
-    A5, B5, C5, D5, E5, F5, G5, H5,
-    A4, B4, C4, D4, E4, F4, G4, H4,
-    A3, B3, C3, D3, E3, F3, G3, H3,
-    A2, B2, C2, D2, E2, F2, G2, H2,
-    A1, B1, C1, D1, E1, F1, G1, H1, }
-
-enum Color { WHITE, BLACK,}
-
-const SQUARE_TO_COORDINATES: [&str; 64] = [
-    "A8", "B8", "C8", "D8", "E8", "F8", "G8", "H8",
-    "A7", "B7", "C7", "D7", "E7", "F7", "G7", "H7",
-    "A6", "B6", "C6", "D6", "E6", "F6", "G6", "H6",
-    "A5", "B5", "C5", "D5", "E5", "F5", "G5", "H5",
-    "A4", "B4", "C4", "D4", "E4", "F4", "G4", "H4",
-    "A3", "B3", "C3", "D3", "E3", "F3", "G3", "H3",
-    "A2", "B2", "C2", "D2", "E2", "F2", "G2", "H2",
-    "A1", "B1", "C1", "D1", "E1", "F1", "G1", "H1"
-];
-
-
-
-
-
-//                                      bit manipulations
-
-//          Bit macros
-macro_rules! get_bit { //returns either 1 or 0, depending on if the square is a active bit
-    ($bb:expr, $square:expr) => {
-        if ($bb & 1u64 << $square) != 0 {1} else {0} // if statement checks if the and operator
-        // between bitboard and square is non zero, checks and returns square bit value
-    }
-}
-macro_rules! set_bit {//sets a bit on a board to a 1
-    ($bb:expr, $square:expr) => {
-        $bb |= 1u64 <<$square // takes the or between the bitboard the the shifted square number
-    }
-}
-macro_rules! pop_bit {//sets a bit on a board to a 0
-    ($bb:expr, $square:expr) => {
-        $bb &= !(1u64 <<$square) // takes the nand between the bitboard and the shifted square
-    }
-}
-
 //use .count_ones to count bits
 //use .trailing_zeros to find least significant bit index
 
-
-
-
-
-fn print_bitboard(bitboard: u64) -> (){ //prints a bitboard
+fn print_bitboard(bitboard: u64) -> () {
+    //prints a bitboard
     println!();
     for rank in 0..8 {
         for file in 0..8 {
@@ -91,49 +31,32 @@ fn print_bitboard(bitboard: u64) -> (){ //prints a bitboard
     println!("   a b c d e f g h");
 
     println!("Bitboard Value: {}", bitboard)
-
 }
 
 //                                              attacks
 
-fn get_bishop_attacks(square: usize, mut occupancy: u64) -> u64 {
-    occupancy &= piececonstants::BISHOP_MASKS[square];
-    occupancy = occupancy.wrapping_mul(piececonstants::BISHOPMAGICNUMBERS[square]);
-    occupancy >>= 64 - piececonstants::BISHOPBITS[square];
-
-    SLIDER_ATTACKS[piececonstants::BISHOP_POINTERS[square] + occupancy as usize]
-}
-
-pub fn get_rook_attacks(square: usize, mut occupancy: u64) -> u64 {
-    occupancy &= piececonstants::ROOK_MASKS[square];
-    occupancy = occupancy.wrapping_mul(piececonstants::ROOKMAGICNUMBERS[square]);
-    occupancy >>= 64 - piececonstants::ROOKBITS[square];
-
-    SLIDER_ATTACKS[piececonstants::ROOK_POINTERS[square] + occupancy as usize]
-}
 //                                              main driver
 
-
-
-
-
-
-
-
-use std::time::{Duration, Instant};
-
-
+//use std::time::{Duration, Instant};
 
 fn main() {
-    let mut occupancy = 0u64;
+    print_bitboard(piececonstants::PAWN_ATTACKS[0][piececonstants::Square::C5 as usize]);
 
-    set_bit!(occupancy, Square::D3 as usize);
-    set_bit!(occupancy, Square::D5 as usize);
-    set_bit!(occupancy, Square::F6 as usize);
+    println!("{}", piececonstants::UNICODE_PIECES[2]);
 
-    print_bitboard(get_rook_attacks(Square::C5 as usize, occupancy));
+    let mut board = engine::Board {
+        ..Default::default()
+    };
+    set_bit!(board.pieceboards[2], 23);
+    set_bit!(board.pieceboards[7], 54);
+    board.side = Some(0);
+    //board.castle = 13;
 
-    print_bitboard(piececonstants::PAWN_ATTACKS[0][Square::C5 as usize]);
+    board.parse_fen("rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq e3 1 2");
+
+    board.print_board();
+
+    //print_bitboard(board.occupancies[0]);
     //let tic = Instant::now();
 
     /*for square in 0..64 {
@@ -141,15 +64,5 @@ fn main() {
     }*/
     //let toc = Instant::now();
 
-
-
-
     //println!("{:#?}", SLIDER_STUFF_2);
-
-
-
-
-
-
-
 }
